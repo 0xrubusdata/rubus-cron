6be483725a32f2ba28f8959922897a240ea23ec0
0xrubusdata <0xrubusdatas@gmail.com>
2025-02-06 17:44:47 +0100

    Updated: config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,10 @@
 export default () => {
   const isDocker = process.env.IN_DOCKER === 'true'; 
-   
+
   return {
     port: Number(process.env.PORT) || 3000,
     database: {
-      host: isDocker ? process.env.POSTGRES_HOST : 'localhost',
+      host: isDocker ? process.env.POSTGRES_HOST : process.env.POSTGRES_LOCALHOST,
       port: Number(process.env.POSTGRES_PORT) || 5432,
       username: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'password',
